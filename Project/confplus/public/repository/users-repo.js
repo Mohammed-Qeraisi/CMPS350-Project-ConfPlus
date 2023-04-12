50cc1637ccfedbc28cb87519326fa1bc82f46bda
Abdelbari-ak1908689 <111994818+Abdelbari-ak1908689@users.noreply.github.com>
2023-04-12 17:19:54 +0300

    Small change

--- Project/confplus/public/repository/users-repo.js
+++ Project/confplus/public/repository/users-repo.js
@@ -8,36 +8,10 @@ class UsersRepo {
     return await response.json();
   }
 
-  //   async deleteAccount(accountNo) {
-  //     const response = await fetch(`${baseUrl}/${accountNo}`, {
-  //       method: "DELETE",
-  //     });
-  //     return await response.json();
-  //   }
-
-  //   async addAccount(account) {
-  //     const response = await fetch(`${baseUrl}`, {
-  //       method: "POST",
-  //       body: JSON.stringify(account),
-  //       headers: { "Content-type": "application/json" },
-  //     });
-  //   }
-
-  //   async updateAccount(account) {
-  //     const response = await fetch(`${baseUrl}/${account.accountNo}`, {
-  //       method: "PUT",
-  //       body: JSON.stringify(account),
-  //       headers: { "Content-type": "application/json" },
-  //     });
-  //   }
-
-  //   async addTrans(trans) {
-  //     const response = await fetch(`${baseUrl}/${trans.accountNo}/trans`, {
-  //       method: "POST",
-  //       body: JSON.stringify(trans),
-  //       headers: { "Content-type": "application/json" },
-  //     });
-  //   }
+  async getUserByRole(role) {
+    const response = await fetch(`${baseUrl}/${role}`);
+    return await response.json();
+  }
 }
 
 export default new UsersRepo();
