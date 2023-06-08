a8f9582c5a57522a17cb629dfa9d8ffbe8d22b1e
Ahmed-aa1907019 <106153524+Ahmed-aa1907019@users.noreply.github.com>
2023-06-08 12:37:38 +0300

    report

--- Project/confplus/public/js/main.js
+++ Project/confplus/public/js/main.js
@@ -4,48 +4,49 @@ const isLoggedIn = sessionStorage.getItem("isLoggedIn");
 const CurrentUser = JSON.parse(sessionStorage.getItem("CurrentUser"));
 
 if (isLoggedIn) {
-    loginBtn.textContent = "Logout";
-    addNav();
-    loginBtn.addEventListener("click", () => {
-        if (confirm(`Are you sure you want LogOut ?`)) {
-            sessionStorage.removeItem("isLoggedIn");
-            sessionStorage.removeItem("CurrentUser");
-            window.location.href = "home.html";
-        }
-    });
+  loginBtn.textContent = "Logout";
+  addNav();
+  loginBtn.addEventListener("click", () => {
+    if (confirm(`Are you sure you want LogOut ?`)) {
+      sessionStorage.removeItem("isLoggedIn");
+      sessionStorage.removeItem("CurrentUser");
+      window.location.href = "home.html";
+    }
+  });
 } else {
-    loginBtn.addEventListener("click", () => {
-        window.location.href = "login.html";
-    });
+  loginBtn.addEventListener("click", () => {
+    window.location.href = "login.html";
+  });
 }
 
 function addNav() {
   const userRole = CurrentUser.userRole;
 
-  let newNavLinks = '';
+  let newNavLinks = "";
 
   if (userRole === "organizer") {
-      newNavLinks += `<li><a href="schedule-editor.html">Edit Schedule</a></li>`;
+    newNavLinks += `<li><a href="schedule-editor.html">Edit Schedule</a></li>`;
+    newNavLinks += `<li><a href="statistics.html">Statistics Report</a></li>`;
   } else if (userRole === "reviewer") {
-      newNavLinks += `<li><a href="review.html">Review Papers</a></li>`;
+    newNavLinks += `<li><a href="review.html">Review Papers</a></li>`;
   } else {
-      newNavLinks += `<li><a href="dashboard.html">Dashboard</a></li>`;
-      newNavLinks += `<li><a href="submitPaper.html">Submit Paper</a></li>`;
+    newNavLinks += `<li><a href="dashboard.html">Dashboard</a></li>`;
+    newNavLinks += `<li><a href="submitPaper.html">Submit Paper</a></li>`;
   }
 
   mainNav.querySelector("ul").insertAdjacentHTML("afterbegin", newNavLinks);
 }
 
 function handleFooterNav(page) {
-    switch (page) {
-        case "team":
-            window.location.href = "home.html";
-            break;
-        case "about":
-            window.location.href = "about.html";
-            break;
-        case "schedue":
-            window.location.href = "schedule.html";
-            break;
-    }
+  switch (page) {
+    case "team":
+      window.location.href = "home.html";
+      break;
+    case "about":
+      window.location.href = "about.html";
+      break;
+    case "schedue":
+      window.location.href = "schedule.html";
+      break;
+  }
 }
